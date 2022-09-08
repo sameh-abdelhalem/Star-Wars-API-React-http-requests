a9ace9e829ae307d76abf63c9c65730d2b8e99ee
Sameh <sameh.abdelhaleem95@gmail.com>
2022-09-08 21:28:05 +0200

    add loading paragraph if movies are still getting fetched

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
 
@@ -18,6 +21,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +30,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading ? <MoviesList movies={movies} /> : <p>Loading Movies</p>}
       </section>
     </React.Fragment>
   );
